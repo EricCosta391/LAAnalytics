d98f06a28235f7da8c5cbec11fe027b99b56f950
lab04 <lab04@lab4-14>
2024-09-23 20:43:47 -0300

    add comp

--- LAAnalytics/src/usa/com/nba/atletas/operacoesController.java
+++ LAAnalytics/src/usa/com/nba/atletas/operacoesController.java
@@ -1,5 +1,18 @@
 package usa.com.nba.atletas;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class operacoesController {
+	@FXML
+	private TextField txfnome;
+	@FXML
+	private TextField txfsobrenome;
+	@FXML
+	private TextField txfidade;
+	@FXML
+	private TextField txfposicao;
+	@FXML
+	private TextField txfequipe;
 	
 }
